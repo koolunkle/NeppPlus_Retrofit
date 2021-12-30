c228559b94f163427d52918931139edfeb62cd98
koolunkle <gugi329@daum.net>
2021-12-30 20:44:48 +0900

    RecyclerView 활용 연습  - Adapter 기초 작업 (1)

---
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/adapters/CategoryRecyclerAdapter.kt
@@ -0,0 +1,13 @@
+package com.neppplus.retrofitlibrarypractice.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class CategoryRecyclerAdapter : RecyclerView.Adapter<CategoryRecyclerAdapter.CategoryViewHolder>() {
+
+    // 클래스 내부의 또다른 클래스 (inner class)
+    inner class CategoryViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+    }
+
+}
