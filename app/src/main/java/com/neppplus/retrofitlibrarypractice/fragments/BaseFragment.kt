7a56680525a32e705532f5f4abfa93c682377025
koolunkle <gugi329@daum.net>
2021-12-30 14:35:06 +0900

    프래그먼트 서버 호출 apiService 상속

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/BaseFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/BaseFragment.kt
@@ -3,14 +3,22 @@ package com.neppplus.retrofitlibrarypractice.fragments
 import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.neppplus.retrofitlibrarypractice.api.ServerAPI
+import com.neppplus.retrofitlibrarypractice.api.ServerAPIService
 
 abstract class BaseFragment : Fragment() {
 
     lateinit var mContext: Context
 
+    // 상속 시켜 줄 것 : API 호출 명세를 담은 변수
+    lateinit var apiService: ServerAPIService
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         mContext = requireContext()
+
+        val retrofit = ServerAPI.getRetrofit()
+        apiService = retrofit.create(ServerAPIService::class.java)
     }
 
     abstract fun setupEvents()
