e7ca3940f1e6a8db62871b1fed92fa2f8e5a9cc2
koolunkle <gugi329@daum.net>
2021-12-30 12:29:57 +0900

    회원가입 API 호출 - (4) 화면에서 API 호출  - 응답 돌아오는 코드 추가, 어떤 행동 작성 X

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/SignUpActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/SignUpActivity.kt
@@ -3,6 +3,10 @@ package com.neppplus.retrofitlibrarypractice
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarypractice.databinding.ActivitySignUpBinding
+import com.neppplus.retrofitlibrarypractice.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : BaseActivity() {
 
@@ -22,6 +26,20 @@ class SignUpActivity : BaseActivity() {
             val email = binding.edtEmail.text.toString()
             val password = binding.edtPassword.text.toString()
             val nickname = binding.edtNickname.text.toString()
+
+            apiService.putRequestSignUp(email, password, nickname).enqueue(object :
+                Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+            })
 
         }
 
