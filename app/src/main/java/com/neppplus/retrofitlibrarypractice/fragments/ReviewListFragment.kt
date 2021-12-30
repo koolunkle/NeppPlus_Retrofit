790cc064f8c19ca402543357024787b140f930da
koolunkle <gugi329@daum.net>
2021-12-30 14:06:05 +0900

    DataBinding

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/ReviewListFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/ReviewListFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.neppplus.retrofitlibrarypractice.R
+import com.neppplus.retrofitlibrarypractice.databinding.FragmentReviewListBinding
 
 class ReviewListFragment : Fragment() {
+
+    lateinit var binding: FragmentReviewListBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_review_list, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_review_list, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
