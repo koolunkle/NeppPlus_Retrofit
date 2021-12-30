937e04a7acb0678cdd787b33ff9e3f2e953bc537
koolunkle <gugi329@daum.net>
2021-12-30 12:21:32 +0900

    로그인 응답 처리  - 자동 파싱된 BasicResponse 변수 이용

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.neppplus.retrofitlibrarypractice
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarypractice.databinding.ActivityMainBinding
+import com.neppplus.retrofitlibrarypractice.datas.BasicResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
@@ -21,7 +26,21 @@ class MainActivity : BaseActivity() {
 
             val inputEmail = binding.edtEmail.text.toString()
             val inputPw = binding.edtPassword.text.toString()
-            apiService.postRequestLogin(inputEmail, inputPw)
+
+            apiService.postRequestLogin(inputEmail, inputPw).enqueue(object :
+                Callback<BasicResponse> {
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    val basicResponse = response.body()!!
+                    Toast.makeText(mContext, basicResponse.message, Toast.LENGTH_SHORT).show()
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+            })
 
         }
 
