1ce5c454db45cfaf75fcec04d6d88f942c4af19f
koolunkle <gugi329@daum.net>
2021-12-31 00:31:09 +0900

    RecyclerView Adapter 작업중 (2)  - 몇줄 안내 / 어떤 xml 설정

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/adapters/ReviewRecyclerAdapter.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/adapters/ReviewRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.neppplus.retrofitlibrarypractice.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.retrofitlibrarypractice.R
 import com.neppplus.retrofitlibrarypractice.datas.ReviewData
 
 class ReviewRecyclerAdapter(
@@ -17,14 +19,15 @@ class ReviewRecyclerAdapter(
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewViewHolder {
 
+        val row = LayoutInflater.from(mContext).inflate(R.layout.review_list_item, parent, false)
+        return ReviewViewHolder(row)
+
     }
 
     override fun onBindViewHolder(holder: ReviewViewHolder, position: Int) {
 
     }
 
-    override fun getItemCount(): Int {
-
-    }
+    override fun getItemCount() = mList.size
 
 }
