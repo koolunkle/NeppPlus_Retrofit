e63335abde5dfd8acf81a888c005317814de22ed
koolunkle <gugi329@daum.net>
2021-12-30 14:07:48 +0900

    상품 목록 프래그먼트 추가

---
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/ProductListFragment.kt
@@ -0,0 +1,30 @@
+package com.neppplus.retrofitlibrarypractice.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.neppplus.retrofitlibrarypractice.R
+import com.neppplus.retrofitlibrarypractice.databinding.FragmentProductListBinding
+
+class ProductListFragment : Fragment() {
+
+    lateinit var binding: FragmentProductListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_product_list, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+
+}
