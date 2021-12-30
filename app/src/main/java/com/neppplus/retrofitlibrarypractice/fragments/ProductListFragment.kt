d425632f2996f0dd96ed0241efa768658686ba8b
koolunkle <gugi329@daum.net>
2021-12-30 22:50:31 +0900

    서버가 주는 상품 목록 -> ArrayList에 전부 담기

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/ProductListFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/ProductListFragment.kt
@@ -7,10 +7,17 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarypractice.R
 import com.neppplus.retrofitlibrarypractice.databinding.FragmentProductListBinding
+import com.neppplus.retrofitlibrarypractice.datas.BasicResponse
+import com.neppplus.retrofitlibrarypractice.datas.ProductData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class ProductListFragment : BaseFragment() {
 
     lateinit var binding: FragmentProductListBinding
+
+    val mProductList = ArrayList<ProductData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,6 +40,23 @@ class ProductListFragment : BaseFragment() {
     }
 
     override fun setValues() {
+        getProductListFromServer()
+    }
+
+    fun getProductListFromServer() {
+
+        apiService.getRequestProductList().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+                    mProductList.clear()
+                    mProductList.addAll(response.body()!!.data.products)
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
 
     }
 
