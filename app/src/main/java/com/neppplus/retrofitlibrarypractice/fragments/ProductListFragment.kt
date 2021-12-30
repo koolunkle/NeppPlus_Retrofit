5946fc4d85eae8c0fce19b6eca15e6709e8160eb
koolunkle <gugi329@daum.net>
2021-12-30 22:41:39 +0900

    상품 목록 프래그먼트 -> RecyclerView만 남게

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/ProductListFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/ProductListFragment.kt
@@ -6,21 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarypractice.R
-import com.neppplus.retrofitlibrarypractice.adapters.ProductAdapter
 import com.neppplus.retrofitlibrarypractice.databinding.FragmentProductListBinding
-import com.neppplus.retrofitlibrarypractice.datas.BasicResponse
-import com.neppplus.retrofitlibrarypractice.datas.ProductData
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class ProductListFragment : BaseFragment() {
 
     lateinit var binding: FragmentProductListBinding
-
-    val mProductList = ArrayList<ProductData>()
-
-    lateinit var mProductAdapter: ProductAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -43,30 +33,6 @@ class ProductListFragment : BaseFragment() {
     }
 
     override fun setValues() {
-
-        getProductListFromServer()
-
-        mProductAdapter = ProductAdapter(mContext, R.layout.product_list_item, mProductList)
-        binding.productListView.adapter = mProductAdapter
-
-    }
-
-    fun getProductListFromServer() {
-
-        apiService.getRequestProductList().enqueue(object : Callback<BasicResponse> {
-            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
-                if (response.isSuccessful) {
-                    val br = response.body()!!
-                    mProductList.clear()
-                    mProductList.addAll(br.data.products)
-                    mProductAdapter.notifyDataSetChanged()
-                }
-            }
-
-            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
-
-            }
-        })
 
     }
 
