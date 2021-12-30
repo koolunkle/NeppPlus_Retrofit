3abb8233e7a3434f97a01ebaf59411290acdf40b
koolunkle <gugi329@daum.net>
2021-12-31 00:59:36 +0900

    사용자 정보에서도 Date 형태로 파싱  - String -> Date 변환 (시차 보정)

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/datas/UserData.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/datas/UserData.kt
@@ -2,16 +2,19 @@ package com.neppplus.retrofitlibrarypractice.datas
 
 import com.google.gson.annotations.SerializedName
 import java.io.Serializable
+import java.util.*
 
 class UserData(
     var id: Int,
     var email: String,
+    var provider: String,
+    var uid: String?,
 //    서버의 이름표와 변수의 이름이 다를때
     @SerializedName("nick_name")
     var nickname: String,
     @SerializedName("profile_img")
     var profileImageURL: String,
-    var provider: String,
-    var uid: String?,
+    @SerializedName("created_at")
+    var createdAt: Date,
 ) : Serializable {
 }
