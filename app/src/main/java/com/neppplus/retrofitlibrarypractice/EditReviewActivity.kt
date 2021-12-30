172b109394937211ea41d7b32ee01fe434e3f885
koolunkle <gugi329@daum.net>
2021-12-31 00:14:48 +0900

    리뷰 작성 - 기본 정보 표시 완료

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/EditReviewActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/EditReviewActivity.kt
@@ -5,6 +5,8 @@ import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarypractice.databinding.ActivityEditReviewBinding
 import com.neppplus.retrofitlibrarypractice.datas.GlobalData
 import com.neppplus.retrofitlibrarypractice.datas.ProductData
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditReviewActivity : BaseActivity() {
 
@@ -29,6 +31,17 @@ class EditReviewActivity : BaseActivity() {
 
         binding.txtProductName.text = mProductData.name
         binding.txtUserNickname.text = GlobalData.loginUser!!.nickname
+
+//        오늘 날짜 -> 2021.5.9 형태로 가공 -> 텍스트뷰에 반영
+
+//        1. 오늘 날짜?
+        val now = Calendar.getInstance() // 현재 일시 자동 기록
+
+//        원하는 형태로 가공 (String 생성)
+        val sdf = SimpleDateFormat("yyyy.M.d")
+        val nowString = sdf.format(now.time)
+
+        binding.txtToday.text = nowString
 
     }
 
