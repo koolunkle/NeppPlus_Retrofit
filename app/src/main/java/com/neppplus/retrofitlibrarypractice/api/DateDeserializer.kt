7d1b91d9de32f8cc21bf918d13ebf5829a2c129a
koolunkle <gugi329@daum.net>
2021-12-31 00:56:28 +0900

    Date 파싱 시 시차도 같이 보정해서 내보내도록

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/api/DateDeserializer.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/api/DateDeserializer.kt
@@ -22,6 +22,12 @@ class DateDeserializer : JsonDeserializer<Date> {
         val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
         val date = sdf.parse(dateStr)!!
 
+//        만들어진 date 변수에는 -> 서버가 알려준 시간(GMT +0)이 분석되어 들어감
+//        한국 폰은 GMT+9로 세팅되어있음(맞춰주자)
+
+        val now = Calendar.getInstance()
+        date.time += now.timeZone.rawOffset // 시차를 ms 단위로 계산한 값
+
 //        파싱 결과로 완성된 date 선정
         return date
 
