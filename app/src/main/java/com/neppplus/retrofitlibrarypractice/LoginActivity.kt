4f6a8e7c8614e3183b0bc94342d964d360787503
koolunkle <gugi329@daum.net>
2021-12-30 14:22:32 +0900

    토큰 받아서 저장하는 기능 추가

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/LoginActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/LoginActivity.kt
@@ -159,6 +159,9 @@ class LoginActivity : BaseActivity() {
                                                     "${br.data.user.nickname}님, 환영합니다!",
                                                     Toast.LENGTH_SHORT
                                                 ).show()
+
+//                                                토큰 값 추출 -> 기기에 저장 (SharedPreferences)
+
                                                 val myIntent =
                                                     Intent(mContext, MainActivity::class.java)
                                                 startActivity(myIntent)
---
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/utils/ContextUtil.kt
@@ -0,0 +1,23 @@
+package com.neppplus.retrofitlibrarypractice.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object {
+
+        // 어떤 파일? 파일명
+        private val prefName = "RetrofitTestPref"
+
+        // 어떤 데이터 항목? 항목명
+        private val TOKEN = "TOKEN"
+
+        // 저장 함수 (setter) / 조회 함수 (getter)
+        fun setToken(context: Context, token: String) {
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putString(TOKEN, token).apply()
+        }
+
+    }
+
+}
