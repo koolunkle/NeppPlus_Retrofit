8f1c4f15a7f14348f605cec53447d54cf1cd6a48
koolunkle <gugi329@daum.net>
2021-12-31 21:52:03 +0900

    Multipart 방식 파일 첨부에 대응하는 APIService 함수 추가   - @multipart / @part 사용

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/api/ServerAPIService.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/api/ServerAPIService.kt
@@ -1,6 +1,7 @@
 package com.neppplus.retrofitlibrarypractice.api
 
 import com.neppplus.retrofitlibrarypractice.datas.BasicResponse
+import okhttp3.MultipartBody
 import retrofit2.Call
 import retrofit2.http.*
 
@@ -72,5 +73,12 @@ interface ServerAPIService {
     // 전체 리뷰 목록 (임시)
     @GET("/review")
     fun getRequestReview(): Call<BasicResponse>
+
+    // 프로필 사진 첨부 -> Field 대신 Multipart 활용
+    @Multipart
+    @PUT("/user/image")
+    fun putRequestProfileImg(
+        @Part img: MultipartBody.Part
+    ): Call<BasicResponse>
 
 }
