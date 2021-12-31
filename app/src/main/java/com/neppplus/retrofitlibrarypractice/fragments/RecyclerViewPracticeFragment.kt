10564884cfabca7705197309f54070c18c31b107
koolunkle <gugi329@daum.net>
2021-12-31 22:22:36 +0900

    서버에서 리뷰 목록 받아서 추가

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/RecyclerViewPracticeFragment.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/fragments/RecyclerViewPracticeFragment.kt
@@ -7,10 +7,17 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.neppplus.retrofitlibrarypractice.R
 import com.neppplus.retrofitlibrarypractice.databinding.FragmentRecyclerviewPracticeBinding
+import com.neppplus.retrofitlibrarypractice.datas.BasicResponse
+import com.neppplus.retrofitlibrarypractice.datas.ReviewData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class RecyclerViewPracticeFragment : BaseFragment() {
 
     lateinit var binding: FragmentRecyclerviewPracticeBinding
+
+    val mReviewList = ArrayList<ReviewData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,6 +44,24 @@ class RecyclerViewPracticeFragment : BaseFragment() {
     }
 
     override fun setValues() {
+        getReviewListFromServer()
+    }
+
+    fun getReviewListFromServer() {
+
+        apiService.getRequestReview().enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+                if (response.isSuccessful) {
+                    val br = response.body()!!
+                    mReviewList.clear()
+                    mReviewList.addAll(br.data.reviews)
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+            }
+        })
 
     }
 
