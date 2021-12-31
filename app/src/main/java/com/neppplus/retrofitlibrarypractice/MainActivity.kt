0dc0f73b829c8e03c6a3d550143b102542bc6ad7
koolunkle <gugi329@daum.net>
2021-12-31 23:15:56 +0900

    ViewPager 페이지 변경 -> BottomNavigation 메뉴 클릭

--- app/src/main/java/com/neppplus/retrofitlibrarypractice/MainActivity.kt
+++ app/src/main/java/com/neppplus/retrofitlibrarypractice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.neppplus.retrofitlibrarypractice
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.viewpager.widget.ViewPager
 import com.neppplus.retrofitlibrarypractice.adapters.MainViewPagerAdapter
 import com.neppplus.retrofitlibrarypractice.databinding.ActivityMainBinding
 
@@ -19,6 +20,37 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+//        1. 뷰페이저를 옮기면 (이벤트) -> 바텀네비게이션 클릭
+
+        binding.mainViewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+
+//                손으로 옮기는 모든 순간 -> 얼만큼 옮기는지도 확인 가능
+
+            }
+
+            override fun onPageSelected(position: Int) {
+
+//                페이지 선택이 완료 되었을 때
+
+//                바텀네비게이션바의 position 에 맞는 메뉴 클릭
+                binding.mainBottomNavView.menu.getItem(position).isChecked = true
+
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+
+//                가만히 있다가 움직이기 시작하는 등의 상태 변경 이벤트
+
+            }
+        })
+
+//        2. 바텀네비게이션 클릭 -> 뷰페이저 페이지 이동
 
     }
 
